ebafd0f83db3355d604b83bc5a21f60b177d5f04
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-07-20 21:00:50 +0100

    Form Creation and Validation

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ document.addEventListener("DOMContentLoaded",function() {
 
         //Validation logic
         feedbackDiv.style.display = "block"; 
+        
         if (isValid) {
             feedbackDiv.textContent = "Registration successful!";
             feedbackDiv.style.color = "#28a745";
